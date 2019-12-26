ed74cf6ee8717285bf70a671d9b3b06778479f2b
DutGRIFF <dutgriff@dutgriff.com>
2019-12-26 12:33:34 -0600

    Added bulma main nav with mobile burger icon

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,48 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/home">Rocket</router-link>
-      <router-link to="/apogee-determination">Apogee Determination</router-link>
-      <router-link to="/performance-prediction">Performance Prediction</router-link>
-      <router-link to="/drag-prediction">Drag Prediction</router-link>
+    <nav class="navbar" role="navigation" aria-label="main navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <!--<a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+          </a>-->
+          <router-link to="/home" class="navbar-item">Rocket</router-link>
+
+          <a role="button" class="navbar-burger burger" v-bind:class="{ 'is-active': navMenuVisible }" v-on:click="toggleNavMenu" 
+              aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div class="navbar-menu" v-bind:class="{ 'is-active': navMenuVisible }">
+          <div class="navbar-start">
+            <router-link to="/apogee-determination" class="navbar-item">Apogee Determination</router-link>
+            <router-link to="/performance-prediction" class="navbar-item">Performance Prediction</router-link>
+            <router-link to="/drag-prediction" class="navbar-item">Drag Prediction</router-link>
+          </div>
+        </div>
+      </div>
     </nav>
-    <router-view></router-view>
+    <div class="container">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
+  data() {
+    return {
+      navMenuVisible: false
+    }
+  },
+  methods: {
+    toggleNavMenu: function () {
+      this.navMenuVisible = !this.navMenuVisible
+    }
+  }
 }
 </script>
